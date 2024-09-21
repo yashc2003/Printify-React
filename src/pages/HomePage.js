9e87025b1b0f34c8b4af2c52a214879c2b488190
Yash Chaudhari <yashchaudhari859@gmail.com>
2024-09-21 10:29:52 +0530

    create component navbar file

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,10 @@
 import React from 'react';
-
+import Navbar from '../Componant/Navbar';
 function HomePage () {
     return (
         <div>
+        <Navbar/>
+        
         <h1>Wellcome to printify</h1>
         </div>
     )
