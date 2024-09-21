7a9e89ba04de8cf836ae177a97ba1ed39b9f4e86
Yash Chaudhari <yashchaudhari859@gmail.com>
2024-09-21 09:55:16 +0530

    setup react app

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import HomePage from './pages/HomePage';
+
+const router = createBrowserRouter([
+  
+    {
+      path: '/',
+      element: <HomePage />
+    },
+])
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+     <RouterProvider router={router} />
+    
+  </>
+);
---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage () {
+    return (
+        <div>
+        <h1>Wellcome to printify</h1>
+        </div>
+    )
+};
+
+
+export default HomePage;
